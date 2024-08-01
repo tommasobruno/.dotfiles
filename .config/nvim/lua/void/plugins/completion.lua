72d92e9d0bbe8f66ed828387bcfe11741c396dd9
Tommaso Bruno <bruno.tommaso@protonmail.com>
2024-08-01 02:23:42 +0100

    chore: completion.lua to use opts instead of config

--- .config/nvim/lua/void/plugins/completion.lua
+++ .config/nvim/lua/void/plugins/completion.lua
@@ -6,43 +6,43 @@ return {
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lsp",
     {
-	"L3MON4D3/LuaSnip",
-	version = "v2.*",
-	build = "make install_jsregexp"
-    }
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp",
+    },
   },
-  config = function()
+  opts = function()
     local l = require("luasnip")
     local cmp = require("cmp")
 
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-    cmp.setup({
+    return {
       sources = {
-	 { name = "nvim_lsp" },
-	 { name = "nvim_lua" },
-	 { name = "path" },
-	 { name = "buffer" }
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "path" },
+        { name = "buffer" },
       },
 
       mapping = cmp.mapping.preset.insert({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
       }),
 
       snippet = {
-	expand = function(args)
-	  l.lsp_expand(args.body)
-	end
+        expand = function(args)
+          l.lsp_expand(args.body)
+        end,
       },
 
       window = {
-	completion = {
-	  scrollbar = false,
-	  sidePadding = 1
-	},
+        completion = {
+          scrollbar = false,
+          side_padding = 1,
+        },
       },
-    })
-  end
+    }
+  end,
 }
