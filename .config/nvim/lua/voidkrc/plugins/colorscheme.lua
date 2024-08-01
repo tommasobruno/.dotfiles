9cc591f0da77c7cac5c5c28556080fc6ec6609a1
voidkrc <bruno.tommaso@protonmail.com>
2024-08-01 10:37:59 +0100

    chore: use opts and init

--- .config/nvim/lua/voidkrc/plugins/colorscheme.lua
+++ .config/nvim/lua/voidkrc/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
 return {
   "rose-pine/neovim",
   name = "rose-pine",
-  config = function()
-    require("rose-pine").setup({
-      variant = "dark",
-      dark_variant = "main",
-      extend_background_behind_borders = false,
-      enable = {
-        terminal = true,
-      },
-      styles = {
-        transparency = true,
-      },
-    })
+  init = function()
     vim.cmd("colorscheme rose-pine")
   end,
+  opts = {
+    variant = "dark",
+    dark_variant = "main",
+    extend_background_behind_borders = false,
+    enable = {
+      terminal = true,
+    },
+    styles = {
+      transparency = true,
+    },
+  },
 }
