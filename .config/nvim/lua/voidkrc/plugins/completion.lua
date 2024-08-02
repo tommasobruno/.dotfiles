659de945cc96e1a2769dce541e416cc2d5ccb5f4
voidkrc <bruno.tommaso@protonmail.com>
2024-08-02 14:23:47 +0100

    chore: refactor to use lazy opts

--- .config/nvim/lua/voidkrc/plugins/completion.lua
+++ .config/nvim/lua/voidkrc/plugins/completion.lua
@@ -1,17 +1,39 @@
 return {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  lazy = false,
-  priority = 100,
   dependencies = {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-nvim-lua",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lsp",
     "saadparwaiz1/cmp_luasnip",
-    { "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+    "L3MON4D3/LuaSnip",
   },
-  config = function()
-    require("voidkrc.config.completion").config()
+  opts = function()
+    local cmp = require("cmp")
+    local ls = require("luasnip")
+
+    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+    return {
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      },
+
+      mapping = {
+        ["<c-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<c-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<cr>"] = cmp.mapping.confirm({ select = true }),
+      },
+
+      snippet = {
+        expand = function(args)
+          ls.lsp_expand(args.body)
+        end,
+      },
+    }
   end,
 }
---
+++ .config/nvim/lua/voidkrc/plugins/snippets.lua
@@ -0,0 +1,40 @@
+return {
+  "L3MON4D3/LuaSnip",
+  version = "v2.*",
+  build = "make install_jsregexp",
+
+  keys = function()
+    local ls = require("luasnip")
+    return {
+      {
+        "<C-k>",
+        function()
+          if ls.expand_or_jumpable() then
+            ls.expand_or_jump()
+          end
+        end,
+        mode = { "i", "s" },
+        silent = true,
+        noremap = true,
+      },
+      {
+        "<C-j>",
+        function()
+          if ls.jumpable(-1) then
+            ls.jump(-1)
+          end
+        end,
+        mode = { "i", "s" },
+        silent = true,
+        noremap = true,
+      },
+    }
+  end,
+
+  main = "luasnip.config",
+  opts = {
+    history = true,
+    updateevents = "TextChanged,TextChangedI",
+    override_builtin = true,
+  },
+}
